9ba7e482b3c533271919332920597ee6fa13e854
YuBeen-Park <74285061+YuBeen-Park@users.noreply.github.com>
2022-01-25 16:55:29 +0900

    검색 레이아웃 수정, 검색 지도 이미지 / 검색 범위 구분 기능 추가, 팝업창 생성(수정필요)

--- app/src/main/java/com/example/guru2_team21/SearchActivity.kt
+++ app/src/main/java/com/example/guru2_team21/SearchActivity.kt
@@ -7,8 +7,8 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
-    var search_area = "nowon"
-    var search_type = "place"
+    var search_area = "nowon"   //검색 지역
+    var search_type = "place"   //장소 or 식당
 
     lateinit var search_place: TextView
     lateinit var search_restaurant: TextView
@@ -17,6 +17,7 @@ class SearchActivity : AppCompatActivity() {
     lateinit var search_hagye: ImageView
     lateinit var search_wolgye: ImageView
     lateinit var search_gongneung: ImageView
+    lateinit var search_by_name: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +30,11 @@ class SearchActivity : AppCompatActivity() {
         search_hagye = findViewById(R.id.search_Hagye)
         search_wolgye = findViewById(R.id.search_Wolgye)
         search_gongneung = findViewById(R.id.search_Gongneung)
+        search_by_name = findViewById(R.id.search_by_name)
 
+        print_info(search_type, search_area)
+
+        //장소 or 식당
         search_place.setOnClickListener {
             search_place.setTextColor(Color.BLACK)
             search_restaurant.setTextColor(Color.GRAY)
@@ -44,16 +49,131 @@ class SearchActivity : AppCompatActivity() {
             print_info(search_type, search_area)
         }
 
+        //이름으로 검색
+        search_by_name.setOnClickListener {
+            val dlg = SearchDialog(this)
+            dlg.start()
+        }
+
+        //검색지역 변경
         search_sanggye.setOnClickListener {
             if(search_area.equals("sanggye")) {
                 search_area = "nowon"
-                //크기 20 줄이기
+                manageImage()
+                print_info(search_type, search_area)
+            } else {
+                search_area = "sanggye"
+                manageImage()
+                print_info(search_type, search_area)
             }
         }
+
+        search_junggye.setOnClickListener {
+            if(search_area.equals("junggye")) {
+                search_area = "nowon"
+                manageImage()
+                print_info(search_type, search_area)
+            } else {
+                search_area = "junggye"
+                manageImage()
+                print_info(search_type, search_area)
+            }
+        }
+
+        search_hagye.setOnClickListener {
+            if(search_area.equals("hagye")) {
+                search_area = "nowon"
+                manageImage()
+                print_info(search_type, search_area)
+            } else {
+                search_area = "hagye"
+                manageImage()
+                print_info(search_type, search_area)
+            }
+        }
+
+        search_wolgye.setOnClickListener {
+            if(search_area.equals("wolgye")) {
+                search_area = "nowon"
+                manageImage()
+                print_info(search_type, search_area)
+            } else {
+                search_area = "wolgye"
+                manageImage()
+                print_info(search_type, search_area)
+            }
+        }
+
+        search_gongneung.setOnClickListener {
+            if(search_area.equals("gongneung")) {
+                search_area = "nowon"
+                manageImage()
+                print_info(search_type, search_area)
+            } else {
+                search_area = "gongneung"
+                manageImage()
+                print_info(search_type, search_area)
+            }
+        }
+
     }
 
     private fun print_info(search_type: String, search_area: String) {
         //정보 search_layout 에 추가하기
+
+    }
+
+    private fun manageImage() {
+        when {
+            search_area.equals("nowon") -> {
+                search_sanggye.drawable.setAlpha(255)
+                search_junggye.drawable.setAlpha(255)
+                search_hagye.drawable.setAlpha(255)
+                search_wolgye.drawable.setAlpha(255)
+                search_gongneung.drawable.setAlpha(255)
+            }
+
+            search_area.equals("sanggye") -> {
+                search_sanggye.drawable.setAlpha(255)
+                search_junggye.drawable.setAlpha(50)
+                search_hagye.drawable.setAlpha(50)
+                search_wolgye.drawable.setAlpha(50)
+                search_gongneung.drawable.setAlpha(50)
+            }
+
+            search_area.equals("junggye") -> {
+                search_sanggye.drawable.setAlpha(50)
+                search_junggye.drawable.setAlpha(255)
+                search_hagye.drawable.setAlpha(50)
+                search_wolgye.drawable.setAlpha(50)
+                search_gongneung.drawable.setAlpha(50)
+            }
+
+            search_area.equals("hagye") -> {
+                search_sanggye.drawable.setAlpha(50)
+                search_junggye.drawable.setAlpha(50)
+                search_hagye.drawable.setAlpha(255)
+                search_wolgye.drawable.setAlpha(50)
+                search_gongneung.drawable.setAlpha(50)
+            }
+
+            search_area.equals("wolgye") -> {
+                search_sanggye.drawable.setAlpha(50)
+                search_junggye.drawable.setAlpha(50)
+                search_hagye.drawable.setAlpha(50)
+                search_wolgye.drawable.setAlpha(255)
+                search_gongneung.drawable.setAlpha(50)
+            }
+
+            search_area.equals("gongneung") -> {
+                search_sanggye.drawable.setAlpha(50)
+                search_junggye.drawable.setAlpha(50)
+                search_hagye.drawable.setAlpha(50)
+                search_wolgye.drawable.setAlpha(50)
+                search_gongneung.drawable.setAlpha(255)
+            }
+        }
+
     }
 
 
