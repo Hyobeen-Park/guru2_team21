24121b3cf9a998b133cec3a29f23ed3bbe9b6131
Ugyeong <dnrud0715@swu.ac.kr>
2022-02-05 00:22:35 +0900

    중간 수정

---
+++ app/src/main/java/com/example/guru2_team21/route_recyclerAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.guru2_team21
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class route_recyclerAdapter (val routelist : ArrayList<routesData>) :
+RecyclerView.Adapter<route_recyclerAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): route_recyclerAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_route_recycler, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return routelist.size
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val placeName: TextView = itemView.findViewById(R.id.route_recyclertext)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.placeName.text = routelist.get(position).name
+        holder.itemView.setOnClickListener {
+
+        }
+    }
+}
